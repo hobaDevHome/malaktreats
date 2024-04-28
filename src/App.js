07ff676a0d0b8c2ce7d2add01184fd31cdce4f14
Heba <heba96@yahoo.com>
2024-04-28 11:37:18 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"; // Import your CSS file
+import header from "./images/s.png";
 
-function App() {
+const defaultTreats = [
+  "50 pounds",
+  "20 pounds",
+  "10 pounds",
+  "use my body butter",
+  "20 mobile rechage",
+];
+
+const App = () => {
+  const [selectedTreat, setSelectedTreat] = useState("");
+  const [treats, setTreats] = useState(defaultTreats);
+  const [newTreatsInput, setNewTreatsInput] = useState("");
+
+  // Function to handle button click
+  const handleButtonClick = (treat) => {
+    setSelectedTreat(treat);
+  };
+  const handleNewTreatsSubmit = () => {
+    const newTreatsArray = newTreatsInput
+      .split("\n")
+      .filter((treat) => treat.trim() !== "");
+    setTreats(newTreatsArray);
+    setNewTreatsInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <img src={header} alt="Header" className="mb-4" />
+        <h1 className="text-4xl font-bold mb-4">Choose a Treat</h1>
+        {selectedTreat && (
+          <p className="text-lg font-medium mb-4">
+            Selected Treat: {selectedTreat}
+          </p>
+        )}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: 10,
+          marginBottom: 10,
+        }}
+      >
+        {treats.map((treat, index) => (
+          <button
+            key={index}
+            onClick={() => handleButtonClick(treat)}
+            className="button"
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <div className="flex flex-col items-center">
+        <textarea
+          rows="4"
+          value={newTreatsInput}
+          onChange={(e) => setNewTreatsInput(e.target.value)}
+          className="w-64 px-4 py-2 border border-gray-300 rounded-lg mb-4"
+          placeholder="Enter new treats (one per line)"
+        />
+        <button onClick={handleNewTreatsSubmit} className="button">
+          Submit
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
